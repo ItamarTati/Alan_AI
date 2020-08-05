0b2bd59cf0ea2dbc6e57ca4ee731af12924d8ac4
Itamar Tati <itamar.tati1994@gmail.com>
2020-08-05 13:14:56 +0100

    connecting react to alan studio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+//@ts-ignore
+import * as alanBtn from '@alan-ai/alan-sdk-web'
+interface command{
+  command: string 
+}
+const alanKey: string = 'c14ffd86f37efd1f70092413640945c92e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ( { command }: command ) => {
+        if(command === 'testCommand'){
+          alert('This Code was excuted')
+        }
+      }
+    })
+  }, [])
   return (
     <div>
       <h1>Alan AI News Application</h1>
