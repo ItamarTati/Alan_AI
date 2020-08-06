d7f52fbed1ef42867e2d36f1a44aaab513f7357c
Itamar Tati <itamar.tati1994@gmail.com>
2020-08-06 20:25:57 +0100

    Updates

---
+++ precache-manifest.738a70a008e336d607f636749fc2bf08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf615afd74eabc87ad25603bbf0d3a9b",
+    "url": "/alan_ai/index.html"
+  },
+  {
+    "revision": "9e58d9febb1b397bf61b",
+    "url": "/alan_ai/static/css/main.f7f4aa5e.chunk.css"
+  },
+  {
+    "revision": "cd1946ef819cb90caeab",
+    "url": "/alan_ai/static/js/2.5efbffa0.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/alan_ai/static/js/2.5efbffa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e58d9febb1b397bf61b",
+    "url": "/alan_ai/static/js/main.7faa4a67.chunk.js"
+  },
+  {
+    "revision": "2fbb1ee8276632878449",
+    "url": "/alan_ai/static/js/runtime-main.7d07f502.js"
+  }
+]);
