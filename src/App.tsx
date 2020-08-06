4e68049b8387c22b6e545e6199828fa2b9366841
Itamar Tati <itamar.tati1994@gmail.com>
2020-08-06 11:26:54 +0100

    added highlight

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 //@ts-ignore
 import * as alanBtn from '@alan-ai/alan-sdk-web'
 import NEWSCARDS from './components/NewsCards/NewsCards'
+import useStyles from './styles';
+
 interface Article{
   author: string, content: string, description: string, publishedAt: string, source: {id: string, name: string}, title: string, url: string, urlToImage: string
 }
@@ -11,25 +13,34 @@ interface Command{
 }
 
 
+
 const alanKey: string = 'c14ffd86f37efd1f70092413640945c92e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 
 const App: React.FC = () => {
-  const [newsArticles, setNewsArticles] = useState<Article[]>([])
+  const [newsArticles, setNewsArticles] = useState<Article[]>([]);
+  const classes = useStyles();
+  const [activeArticle, setActiveArticle] = useState<number>(-1)
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ( { command, articles }: Command ) => {
         if(command === 'newHeadlines'){
           setNewsArticles(articles) 
+          setActiveArticle(-1)
+        } else if(command === 'highlight'){
+          setActiveArticle((prevActiveArticle)=> prevActiveArticle + 1)
         }
       }
     })
   }, [])
+
   return (
     <div>
-      <h1>Alan AI News Application</h1>
-      <NEWSCARDS key = 'news' articles = {newsArticles}/>
+      <div className = {classes.logoContainer}>
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo}alt ="alan logo"/>
+      </div>
+      <NEWSCARDS key = 'news' articles = {newsArticles} activeArticle = {activeArticle}/>
     </div>
   );
 }
--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -3,37 +3,38 @@ import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typo
 import classNames from 'classnames';
 import useStyles from './styles';
 
-interface Props{
+interface Props {
     article: Article,
     index: number
+    activeArticle: number
 }
-interface Article{
-    author: string, content: string, description: string, publishedAt: string, source: {id: string, name: string}, title: string, url: string, urlToImage: string
-  }
+interface Article {
+    author: string, content: string, description: string, publishedAt: string, source: { id: string, name: string }, title: string, url: string, urlToImage: string
+}
 
-  const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, index}: Props) => {
-    const classes= useStyles()  
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, index, activeArticle }: Props) => {
+    const classes = useStyles()
     return (
-      <Card className={classes.card}>
-          <CardActionArea href ={url} target="_blank">
-              <CardMedia className={classes.media} image ={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}/>
-              <div className={classes.details}>
-                  <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-                  <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
-              </div>
-              <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
-              <CardContent>
-                  <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
-              </CardContent>
-          </CardActionArea>
-          <CardActions className={classes.cardActions}>
-              <Button size="small" color="primary" href=''>Learn More</Button>
-              <Typography variant="h5" color="textSecondary" component="h2">{index + 1}</Typography>
-          </CardActions>
+        <Card className={classNames(classes.card, activeArticle === index ? classes.activeCard : null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} />
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" href=''>Learn More</Button>
+                <Typography variant="h5" color="textSecondary" component="h2">{index + 1}</Typography>
+            </CardActions>
 
-      </Card>
-      )
-  }
+        </Card>
+    )
+}
 
 
 export default NewsCard;
